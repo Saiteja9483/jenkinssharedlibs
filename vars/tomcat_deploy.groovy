3c4875a10449bdc13295e0f3c803e8f8518bd882
Saiteja9483 <106792499+Saiteja9483@users.noreply.github.com>
2022-08-24 15:51:27 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -1,5 +1,5 @@
-def call(tomcat_deploy,user,ip){
-  sshagent([${'tomcat_deploy'}]) {
+def call(credId,user,ip){
+  sshagent([credId]) {
                          sh "mv target/myweb*.war target/app.war"
     sh "scp -o StrictHostKeyChecking=no target/app.war ${user}@${ip}:/opt/tomcat/webapps "
     sh "ssh ${user}@${ip} sudo /opt/tomcat/bin/startup.sh"
